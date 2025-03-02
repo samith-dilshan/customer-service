f28f9159f3a9a0da89d0595a21e768dfdf3c58e0
samith-dilshan <my.koyeb.ac@gmail.com>
2025-03-02 21:02:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
     prefix: ".", //Enter bot prefix here
-    alive_msg: "*ᴛʜᴀʀᴜᴜx-ᴍᴅ* \n\n```Hey! I'm alive...``` \n\nType `.menu` for list of commands_ \n\n\n> Developed by THARUUX", //Enter the alive msg you want to send.
-    alive_img_url: "https://github.com/THARUUX/THARUUX/blob/main/20240509_131601.png?raw=true", //Enter an alive image you want to send
-    userJID: '@s.whatsapp.net', //Replace this with you number with country code ex: 94723345678 
+    alive_msg: "HI Dear, I'm online\n\n- Please use *.menu* to get menu\n\n> Customer Service - Chatzy", //Enter the alive msg you want to send.
+    alive_img_url: "https://i.ibb.co/p6BHRTxn/5124556.jpg", //Enter an alive image you want to send
+    userJID: '94785760531', //Replace this with you number with country code ex: 94723345678 
 };
