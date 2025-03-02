9f9af0a3fe1709eb8547e931cb4d1b25dc8b18f6
samith-dilshan <my.koyeb.ac@gmail.com>
2025-03-02 21:04:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
     prefix: ".", //Enter bot prefix here
     alive_msg: "HI Dear, I'm online\n\n- Please use *.menu* to get menu\n\n> Customer Service - Chatzy", //Enter the alive msg you want to send.
     alive_img_url: "https://i.ibb.co/p6BHRTxn/5124556.jpg", //Enter an alive image you want to send
-    userJID: '94785760531', //Replace this with you number with country code ex: 94723345678 
+    userJID: '94785760531@s.whatsapp.net', //Replace this with you number with country code ex: 94723345678 
 };
